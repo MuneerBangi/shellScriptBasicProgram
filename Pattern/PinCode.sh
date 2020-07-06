fba8b15bec1ed058a8a717f4a0efff39764397fc
MuneerBangi <muneerbangi1@gmail.com>
2020-07-06 20:16:35 +0530

    Pin Code Program

---
+++ Pattern/PinCode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+shopt -s extglob
+
+#pat="^[1-9][0-9]{5}$"
+pat="^[1-9][0-9]{2} {0,1}[0-9]{3}$"
+read -p "Enter pin code:" pin
+
+if [[ $pin =~ $pat ]]
+then
+echo "yes"
+else
+echo "no"
+fi
